4bbcb9607d9e765319bf78915d9dd1872a771a53
Kieranties <kieran@kieranties.com>
2011-12-01 13:58:53 +0000

    Moved configuration setup to inner class to prevent exposure of namespaces

--- Distribution/CSharp/Nodle.Distribution/NodleDistributionConfig.cs
+++ Distribution/CSharp/Nodle.Distribution/NodleDistributionConfig.cs
@@ -5,45 +5,12 @@ namespace Nodle.Distribution
     /// <summary>
     /// General confgiration object
     /// </summary>
-    public class NodleDistributionConfig : ConfigurationSection, INodleDistributionConfig
+    public class NodleDistributionConfig
     {
-        private static NodleDistributionConfig _instance;
+        private static NodleDistributionConfigSection _instance;
         private static readonly string _defaultSection = "nodleDistribution";
         internal static string _customSection { get; set; }
-
-        /// <summary>
-        /// Gets the nodle service username.
-        /// </summary>
-        [ConfigurationProperty("accessKey", IsRequired = true)]
-        public string AccessKey
-        {
-            get
-            {
-                return (string)this["accessKey"];
-            }
-        }
         
-        /// <summary>
-        /// Gets the nodle service URL.
-        /// </summary>
-        [ConfigurationProperty("baseUrl", IsRequired = true)]
-        public string BaseUrl
-        {
-            get
-            {
-                return (string)this["baseUrl"];
-            }
-        }
-
-        /// <summary>
-        /// Gets the nodle service channel.
-        /// </summary>
-        [ConfigurationProperty("channel", IsRequired = true)]
-        public string Channel
-        {
-            get { return (string) this["channel"]; }
-        }
-
         /// <summary>
         /// Returns the config instance.
         /// </summary>
@@ -53,10 +20,49 @@ namespace Nodle.Distribution
             {
                 if(_instance == null)
                 {
-                    _instance = ConfigurationManager.GetSection(_customSection ?? _defaultSection ) as NodleDistributionConfig;
+                    _instance = ConfigurationManager.GetSection(_customSection ?? _defaultSection) as NodleDistributionConfigSection;
                 }
 
                 return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Inner model representation of config section
+        /// </summary>
+        private class NodleDistributionConfigSection : ConfigurationSection, INodleDistributionConfig
+        {
+            /// <summary>
+            /// Gets the nodle service username.
+            /// </summary>
+            [ConfigurationProperty("accessKey", IsRequired = true)]
+            public string AccessKey
+            {
+                get
+                {
+                    return (string)this["accessKey"];
+                }
+            }
+
+            /// <summary>
+            /// Gets the nodle service URL.
+            /// </summary>
+            [ConfigurationProperty("baseUrl", IsRequired = true)]
+            public string BaseUrl
+            {
+                get
+                {
+                    return (string)this["baseUrl"];
+                }
+            }
+
+            /// <summary>
+            /// Gets the nodle service channel.
+            /// </summary>
+            [ConfigurationProperty("channel", IsRequired = true)]
+            public string Channel
+            {
+                get { return (string)this["channel"]; }
             }
         }
     }
