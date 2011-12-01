d14c5e2bd8961faa22950b1f8badd3fd0f09ec9f
Kieranties <kieran@kieranties.com>
2011-12-01 14:29:31 +0000

    Implemented message model/interface

--- Distribution/CSharp/Nodle.Distribution/INodleMessage.cs
+++ Distribution/CSharp/Nodle.Distribution/INodleMessage.cs
@@ -1,6 +1,17 @@
+using System;
+using Newtonsoft.Json;
+
 namespace Nodle.Distribution
 {
+    /// <summary>
+    /// Message interface
+    /// </summary>
     public interface INodleMessage
     {
+        [JsonProperty("created")]
+        DateTime Created { get; }
+
+        [JsonProperty("body")]
+        object Body { get; set; }
     }
 }
---
+++ Distribution/CSharp/Nodle.Distribution/NodleMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nodle.Distribution
+{
+    /// <summary>
+    /// Simple implementation of a message
+    /// </summary>
+    public class NodleMessage: INodleMessage
+    {
+        /// <summary>
+        /// Gets the time the send created the message
+        /// </summary>
+        public DateTime Created { get; private set; }
+
+        /// <summary>
+        /// The message body.  Contents will vary based on the object sent
+        /// </summary>
+        public object Body { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NodleMessage"/> class.
+        /// </summary>
+        public NodleMessage()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}
