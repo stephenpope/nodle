091f8500b9f21286a152552b8a5e76abe1215ce8
Kieranties <kieran@kieranties.com>
2011-12-01 21:30:09 +0000

    Updated 'cre' refs to 'lib', updated env refs to static for testing in node at command line

--- node/app.js
+++ node/app.js
@@ -5,11 +5,11 @@
 var express = require('express'),
 	app = module.exports = express.createServer(),
   couchParams = {
-    protocol: process.env.couchProtocol,
-    url: process.env.couchUrl,
-    name: process.env.couchDb,
-    username: process.env.couchUsername,
-    password: process.env.couchPassword
+    protocol: "https",
+    url: "kieranties.cloudant.com",
+    name: "nodle",
+    username: "kieranties",
+    password: "e9WBb4fP"
   };
 
 // Express configuration
@@ -32,13 +32,14 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
   
-var io = require('./core/socket').init(app),
-  nano = require('./core/couch').init(couchParams);
+var io = require('./lib/socket').init(app),
+  nano = require('./lib/couch').init(couchParams);
 
 //init routes modules
-require('./core/routes').init(app, nano, io, __dirname + '/public');
+require('./lib/routes').init(app, nano, io, __dirname + '/public');
 
 // Running on iisnode so use port from global process (iis defines the port)
-app.listen(process.env.PORT);
-console.log("Nodle server listening in %s mode", app.settings.env);
+//app.listen(process.env.PORT);
+app.listen(1337,"192.168.0.8");
+//console.log("Nodle server listening in %s mode", app.settings.env);
 
