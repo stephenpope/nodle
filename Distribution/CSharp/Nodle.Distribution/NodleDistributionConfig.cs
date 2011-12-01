80886d7dd75a386934268884d563d3798d4e4792
Kieranties <kieran@kieranties.com>
2011-12-01 12:18:56 +0000

    Added distribution config model

---
+++ Distribution/CSharp/Nodle.Distribution/NodleDistributionConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Configuration;
+
+namespace Nodle.Distribution
+{
+    /// <summary>
+    /// General confgiration object
+    /// </summary>
+    public class NodleDistributionConfig : ConfigurationSection
+    {
+        /// <summary>
+        /// Gets the nodle service username.
+        /// </summary>
+        [ConfigurationProperty("username", IsRequired = true)]
+        public string Username
+        {
+            get
+            {
+                return (string)this["username"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the nodle service password.
+        /// </summary>
+        [ConfigurationProperty("password", IsRequired = true)]
+        public string Password
+        {
+            get
+            {
+                return (string)this["password"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the nodle service URL.
+        /// </summary>
+        [ConfigurationProperty("url", IsRequired = true)]
+        public string Url
+        {
+            get
+            {
+                return (string)this["url"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the nodle service channel.
+        /// </summary>
+        [ConfigurationProperty("channel", IsRequired = true)]
+        public string Channel
+        {
+            get { return (string) this["channel"]; }
+        }
+    }
+}
