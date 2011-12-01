ffb5f2668b9c2f58701f47e00791b56d97dca2af
Kieranties <kieran@kieranties.com>
2011-12-01 12:36:11 +0000

    Added first tests for config model

---
+++ Distribution/CSharp/Nodle.Distribution.Tests/NodleDistributionConfigFixture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nodle.Distribution.Tests
+{
+    [TestFixture]
+    public class NodleDistributionConfigFixture
+    {
+        private NodleDistributionConfig _config;
+        private string _username = "TestyMcTesterson";
+        private string _password = "MyPasswordIsRubbish";
+        private string _url = "http://nodletestaddress";
+        private string _channel = "TheWorld";
+
+        [SetUp]
+        public void Setup()
+        {
+            _config = new NodleDistributionConfig(_username, _password, _url, _channel);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _config = null;
+        }
+
+        [Test]
+        public void Get_Username_ReturnsGivenValue()
+        {
+            Assert.AreEqual(_username, _config.Username);
+        }
+
+        [Test]
+        public void Get_Password_ReturnsGivenValue()
+        {
+            Assert.AreEqual(_password, _config.Password);
+        }
+
+        [Test]
+        public void Get_Url_ReturnsGivenValue()
+        {
+            Assert.AreEqual(_url, _config.Url);
+        }
+
+        [Test]
+        public void Get_Channel_ReturnsGivenValue()
+        {
+            Assert.AreEqual(_channel, _config.Channel);
+        }
+    }
+}
