ccea5c5ae085ad09736bd64f3ff054c3830570f8
Kieranties <kieran@kieranties.com>
2011-12-01 17:36:19 +0000

    Updated routes for channel publishing

--- node/core/routes.js
+++ node/core/routes.js
@@ -1,13 +1,12 @@
 /*
     Route configuration
  */
-exports.init = function(app, publicPath) {
+exports.init = function(app, nano, io, publicPath) {
     //properties
     var title = "Nodle";
 
     // Get index
     app.get('/', function(req, res) {
-
         res.render('index', {
             title: title,
             crumbs: [
@@ -28,4 +27,30 @@ exports.init = function(app, publicPath) {
         }
         res.json({children:content});
     });
+
+    app.post('/pub/:channel', function(req, res){
+        var id = req.header('nodleMsgId');
+        var channel = req.params.channel;
+        var content = req.body;
+        
+        console.log(req.is('json'));
+        console.log(req);
+        //store message
+        nano.store(channel, id, content, function(data){
+            res.json({
+                id: id,
+                channel: channel,
+                content: data
+            })
+        });
+
+        io.send(content, function(){
+            console.log('sent');
+        });
+        //distribute message
+    })
+
+    app.get('/channels', function(req, res){
+        res.json(nano.channels);
+    })
 };
